8e130ab0f076a2aade2003d321e8a9573c947b93
ndminh1307js <ndminh1307js@gmail.com>
2020-10-31 13:17:32 +0700

    Chapter 11: Dynamically creating properties

--- chapter-11/src/index.ts
+++ chapter-11/src/index.ts
@@ -1,19 +1,6 @@
-interface Person {
-  name: string;
-  getDetails(): string;
-}
-
 interface Product {
   name: string;
   price: number;
-}
-
-class Employee implements Person {
-  constructor(public name: string, public company: string) { }
-
-  getDetails() {
-    return `${this.name} works for ${this.company}`;
-  }
 }
 
 class SportsProduct implements Product {
@@ -21,16 +8,16 @@ class SportsProduct implements Product {
     public price: number) { }
 }
 
-let data: (Person | Product)[] = [
-  new Employee('Harry Potter', 'Hogwarts'),
-  new SportsProduct('Mjolnir', 'Hammer', 100),
-  new Employee('Hermione', 'Hogwarts')
-];
-
-data.forEach(item => {
-  if ('getDetails' in item) {
-    console.log(`Person: ${item.getDetails()}`);
-  } else {
-    console.log(`Product: ${item.name}, ${item.price}`);
+class ProductGroup {
+  constructor(...initialProducts: [string, Product][]) {
+    initialProducts.forEach(p => this[p[0]] = p[1]);
   }
-});
+
+  [propertyName: string]: Product;
+}
+
+let group = new ProductGroup(
+  ['shoe', new SportsProduct('Shoe', 'Running', 90.05)],
+  ['hat', new SportsProduct('Hat', 'Skiing', 20)]
+);
+Object.keys(group).forEach(k => console.log(`Property Name: ${k}`));
