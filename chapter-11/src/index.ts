a67a9ef492f57085c177437f35780e8c94d228e9
ndminh1307js <ndminh1307js@gmail.com>
2020-10-31 12:53:01 +0700

    Chapter 11: Implement interface

--- chapter-11/src/index.ts
+++ chapter-11/src/index.ts
@@ -1,63 +1,44 @@
-abstract class Person {
-  constructor(
-    public id: string,
-    public name: string,
-    public city: string
-  ) { }
-
-  getDetails(): string {
-    return `${this.name}, ${this.getSpecificDetails()}`;
-  }
-
-  abstract getSpecificDetails(): string;
+interface Person {
+  name: string;
+  getDetails(): string
 }
 
-class Employee extends Person {
+class Employee implements Person {
   constructor(
     public readonly id: string,
     public name: string,
     private dept: string,
     public city: string
-  ) {
-    super(id, name, city);
-  }
+  ) { }
 
-  writeDept() {
-    console.log(`${this.name} works in the ${this.dept} deparment`);
-  }
-
-  getSpecificDetails() {
-    return `works in ${this.dept}`;
+  getDetails() {
+    return `${this.name} works in ${this.dept}`;
   }
 }
 
-class Customer extends Person {
+class Customer implements Person {
   constructor(
     public readonly id: string,
     public name: string,
     public city: string,
     public creditLimit: number
-  ) {
-    super(id, name, city);
-  }
+  ) { }
 
-  getSpecificDetails() {
-    return `has ${this.creditLimit} limit`;
+  getDetails() {
+    return `${this.name} has ${this.creditLimit} limit`;
   }
 }
 
-class Supplier extends Person {
+class Supplier implements Person {
   constructor(
     public readonly id: string,
     public name: string,
     public city: string,
     public companyName: string
-  ) {
-    super(id, name, city);
-  }
+  ) { }
 
-  getSpecificDetails() {
-    return `works for ${this.companyName}`;
+  getDetails() {
+    return `${this.name} works for ${this.companyName}`;
   }
 }
 
