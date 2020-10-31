7d725835bdb7200657049e50d1b7f300d56292d4
ndminh1307js <ndminh1307js@gmail.com>
2020-10-31 13:06:02 +0700

    Chapter 11: Defining Abstract Interface Implementation

--- chapter-11/src/index.ts
+++ chapter-11/src/index.ts
@@ -6,62 +6,37 @@ interface Person {
   getDogDetails?(): string;
 }
 
-class Employee implements Person {
-  constructor(
-    public readonly id: string,
-    public name: string,
-    private dept: string,
-    public city: string
-  ) { }
+abstract class AbstractDogOwner {
+  abstract name: string;
+  abstract dogName?: string;
 
-  getDetails() {
-    return `${this.name} works in ${this.dept}`;
+  abstract getDetails();
+
+  getDogDetails() {
+    if (this.dogName) {
+      return `${this.name} has a dog named ${this.dogName}`;
+    }
   }
 }
 
-class Customer implements Person {
+class DogOwningCustomer extends AbstractDogOwner {
   constructor(
     public readonly id: string,
     public name: string,
     public city: string,
     public creditLimit: number,
     public dogName: string
-  ) { }
+  ) {
+    super();
+  }
 
   getDetails() {
     return `${this.name} has ${this.creditLimit} limit`;
   }
-
-  getDogDetails() {
-    return `${this.name} has a dog named ${this.dogName}`;
-  }
 }
 
-class Supplier implements Person {
-  constructor(
-    public readonly id: string,
-    public name: string,
-    public city: string,
-    public companyName: string
-  ) { }
+let bella = new DogOwningCustomer('bella', 'Isabella Swan', 'Ariana', 500, 'Lulu');
 
-  getDetails() {
-    return `${this.name} works for ${this.companyName}`;
-  }
+if (bella.dogName) {
+  console.log(bella.getDogDetails());
 }
-
-let bella = new Customer('bella', 'Isabella Swan', 'Ariana', 500, 'Lulu');
-
-let data: Person[] = [
-  bella,
-  new Employee('edward', 'Edward Cullen', 'Vampire', 'Ariana')
-];
-
-data.push(new Supplier('kylian', 'Kylian Mbappe', 'Paris', 'PSG'));
-
-data.forEach(item => {
-  console.log(item.getDetails());
-  if (item.getDogDetails) {
-    console.log(item.getDogDetails());
-  }
-});
