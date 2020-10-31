bddc0a4e1aec0c0317ceb0a78db90c1487a36c1b
ndminh1307js <ndminh1307js@gmail.com>
2020-10-31 11:41:41 +0700

    Using Intersections for Data Correlation

--- chapter-10/src/index.ts
+++ chapter-10/src/index.ts
@@ -5,21 +5,38 @@ type Person = {
 };
 
 type Employee = {
+  id: string,
   company: string,
   dept: string
 };
 
-let bob = {
-  id: 'bsmith',
-  name: 'Bob',
-  city: 'London',
-  company: 'Acme Co',
-  dept: 'Sales'
-};
+type EmployedPerson = Person & Employee;
 
-let dataItems: (Person & Employee)[] = [bob];
+function correlatedData(
+  peopleData: Person[],
+  staff: Employee[]
+): EmployedPerson[] {
+  const defaults = { company: 'None', dept: 'None' };
+  return peopleData.map(p => ({
+    ...p,
+    ...staff.find(e => e.id === p.id) || { ...defaults, id: p.id }
+  }));
+}
+
+let people: Person[] = [
+  { id: 'bsmith', name: 'Bob Smith', city: 'London' },
+  { id: 'ajones', name: 'Alice Jones', city: 'Paris' },
+  { id: 'dpeters', name: 'Dora Peters', city: 'New York' }
+];
+
+let employees: Employee[] = [
+  { id: 'bsmith', company: 'Samsung', dept: 'R&D' },
+  { id: 'dpeters', company: 'Apple', dept: 'Marketing' }
+];
+
+let dataItems: EmployedPerson[] = correlatedData(people, employees);
 
 dataItems.forEach(item => {
-  console.log(`Person: ${item.id}, ${item.name}, ${item.city}`);
+  console.log(`- Person: ${item.id}, ${item.name}, ${item.city}`);
   console.log(`Employee: ${item.company}, ${item.dept}`);
 });
