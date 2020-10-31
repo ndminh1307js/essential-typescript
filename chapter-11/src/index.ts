3e0f6f060820d316d14cb6a2c1f5b58e209536d4
ndminh1307js <ndminh1307js@gmail.com>
2020-10-31 12:43:24 +0700

    Chapter 11: Using class inheritance

--- chapter-11/src/index.ts
+++ chapter-11/src/index.ts
@@ -1,17 +1,19 @@
-type Person = {
-  id: string,
-  name: string,
-  city: string
-};
+class Person {
+  constructor(
+    public id: string,
+    public name: string,
+    public city: string
+  ) { }
+}
 
-class Employee {
+class Employee extends Person {
   constructor(
     public readonly id: string,
     public name: string,
     private dept: string,
     public city: string
   ) {
-    // no statements required
+    super(id, name, city);
   }
 
   writeDept() {
@@ -19,5 +21,42 @@ class Employee {
   }
 }
 
-let salesEmployee = new Employee('shinichikudo', 'Conan Edogawa', 'Detective Conan', 'detective');
-salesEmployee.writeDept();
+class Customer extends Person {
+  constructor(
+    public readonly id: string,
+    public name: string,
+    public city: string,
+    public creditLimit: number
+  ) {
+    super(id, name, city);
+  }
+}
+
+class Supplier extends Person {
+  constructor(
+    public readonly id: string,
+    public name: string,
+    public city: string,
+    public companyName: string
+  ) {
+    super(id, name, city);
+  }
+}
+
+let data: Person[] = [
+  new Customer('bella', 'Isabella Swan', 'Ariana', 500),
+  new Employee('edward', 'Edward Cullen', 'Vampire', 'Ariana')
+];
+
+data.push(new Supplier('kylian', 'Kylian Mbappe', 'Paris', 'PSG'));
+
+data.forEach(item => {
+  console.log(`Person: ${item.id} ${item.name}`);
+  if (item instanceof Employee) {
+    item.writeDept();
+  } else if (item instanceof Customer) {
+    console.log(`Customer ${item.name} has ${item.creditLimit} limit`);
+  } else if (item instanceof Supplier) {
+    console.log(`Supplier ${item.name} works for ${item.companyName}`);
+  }
+});
