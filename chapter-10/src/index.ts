e39152d9c56961c045dfce8960de1fcc5e6841e5
ndminh1307js <ndminh1307js@gmail.com>
2020-10-31 11:24:53 +0700

    Using Type Union

--- chapter-10/src/index.ts
+++ chapter-10/src/index.ts
@@ -1,37 +1,25 @@
-enum Feature {
-  Waterproof,
-  Insulated
-}
-
 type Product = {
+  id: number,
   name: string,
-  price?: number,
-  hasFeature?(Feature): boolean
+  price?: number
+};
+
+type Person = {
+  id: string,
+  name: string,
+  city: string
+};
+
+type UnionType = {
+  id: number | string,
+  name: string
 }
 
-let hat = { name: 'Hat', price: 100 };
-let gloves = { name: 'Gloves', price: 75 };
-let umbrella = {
-  name: 'Umbrella',
-  price: 30,
-  hasFeature: (feature) => feature === Feature.Waterproof
-};
-let mirrorShades = {
-  name: 'Sunglasses',
-  price: 54,
-  finish: 'mirrored'
-};
-let darkShades = {
-  name: 'Sunglasses',
-  price: 54,
-  finish: 'flat'
-};
+let dataItems: UnionType[] = [
+  { id: 1, name: 'Hat', price: 100 },
+  { id: 2, name: 'Gloves', price: 75 },
+  { id: 3, name: 'Umbrella', price: 30 },
+  { id: 'bsmith', name: 'Bob', city: 'London' }
+];
 
-let products: Product[] = [hat, gloves, umbrella, mirrorShades, darkShades];
-
-products.forEach(prod => {
-  console.log(`${prod.name}: ${prod.price}`);
-  console.log(`\t- Waterproof: ${prod.hasFeature
-    ? prod.hasFeature(Feature.Waterproof)
-    : false}`);
-});
+dataItems.forEach(item => console.log(`ID: ${item.id}, Name: ${item.name}`));
