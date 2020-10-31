81ddf1cc37278ab5fd6c30b63c2af83815069294
ndminh1307js <ndminh1307js@gmail.com>
2020-11-01 00:06:15 +0700

    Chapter 12: Defining generic interfaces

--- chapter-12/src/index.ts
+++ chapter-12/src/index.ts
@@ -1,49 +1,23 @@
 import { Person, Product, City, Employee } from './dataTypes';
 
-let people = [
-  new Person('James McAvoy', 'Manchester'),
-  new Person('Emma Stones', 'New York')
-];
+type Name = {
+  name: string
+};
 
-let products = [
-  new Product('Hat', 12),
-  new Product('Shoe', 20)
-];
-
-let cities = [
-  new City('New York', 12),
-  new City('Manchester', 15)
-];
-
-let employees = [
-  new Employee('Emma Stones', 'Actress'),
-  new Employee('Ryan Gosling', 'Actor')
-];
-
-class DataCollection<T extends { name: string }> {
-  protected items: T[] = [];
-
-  constructor(initialItems: T[]) {
-    this.items = initialItems;
-  }
-
-  filter<V extends T>(predicate: (target) => target is V): V[] {
-    return this.items.filter(item => predicate(item)) as V[];
-  }
-
-  static reverse<ArrayType>(items: ArrayType[]): ArrayType[] {
-    return items.reverse();
-  }
+interface Collection<T extends Name> {
+  add(...newItems: T[]): void;
+  get(name: string): T;
+  count: number;
 }
 
-let mixedData = new DataCollection<Person | Product>([...people, ...products]);
-
-function isProduct(target): target is Product {
-  return target instanceof Product;
+interface SearchableCollection<T extends Name> extends Collection<T> {
+  find(name: string): T | undefined;
 }
 
-let filteredProducts = mixedData.filter(isProduct);
-console.log(filteredProducts);
+interface ProductCollection extends Collection<Product> {
+  sumPrices(): number;
+}
 
-let reversedCities = DataCollection.reverse<City>(cities);
-console.log(reversedCities);
+interface PeopleCollection<T extends Person | Employee> extends Collection<T> {
+  getNames(): string[]
+}
