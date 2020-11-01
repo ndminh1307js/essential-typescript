ffa8e9e91a4b7b12ff4b72994c15fa5a952891ec
ndminh1307js <ndminh1307js@gmail.com>
2020-11-01 08:59:08 +0700

    Chapter 13: Combining transformations in a single mapping

--- chapter-13/src/index.ts
+++ chapter-13/src/index.ts
@@ -1,10 +1,15 @@
 import { Person, Product, City, Employee } from './dataTypes';
 
-type SelectProperties<T, K extends keyof T> = {
-	[P in K]: T[P];
+type CustomMapped<T, K extends keyof T> = {
+	readonly [P in K]?: T[P];
 };
 
-let p1: SelectProperties<Product, 'name'> = { name: 'Kayak' };
-let p2: Pick<Product, 'name' | 'price'> = { name: 'Lifejacket', price: 48.95 };
+type BuiltInMapped<T, K extends keyof T> = Readonly<Partial<Pick<T, K>>>;
+
+let p1: CustomMapped<Product, 'name'> = { name: 'Kayak' };
+let p2: BuiltInMapped<Product, 'name' | 'price'> = {
+	name: 'Lifejacket',
+	price: 48.95,
+};
 console.log(`Custom mapped type: ${p1.name}`);
 console.log(`Built-in mapped type: ${p2.name}, ${p2.price}`);
