615133a5ae8ba5df099cfcc6289c908eee579b9a
ndminh1307js <ndminh1307js@gmail.com>
2020-11-01 08:29:40 +0700

    Chapter 13: Using index type & indexed access operator

--- chapter-13/src/index.ts
+++ chapter-13/src/index.ts
@@ -2,36 +2,17 @@ import { Person, Product, City, Employee } from './dataTypes';
 
 let products = [new Product('Running Shoe', 25), new Product('Hat', 12)];
 
-type shapeType = { name: string };
-
-class Collection<T extends shapeType> implements Iterable<T> {
-	private items: Map<string, T>;
-
-	constructor(initialItems: T[] = []) {
-		this.items = new Map<string, T>();
-		this.add(...initialItems);
-	}
-
-	add(...newItems: T[]): void {
-		newItems.forEach((newItem) => this.items.set(newItem.name, newItem));
-	}
-
-	get(name: string): T | undefined {
-		return this.items.get(name);
-	}
-
-	get count(): number {
-		return this.items.size;
-	}
-
-	[Symbol.iterator](): IterableIterator<T> {
-		return this.items.values();
-	}
+function getValue<T, K extends keyof T>(item: T, keyname: K): T[K] {
+	return item[keyname];
 }
 
-let productCollection: Collection<Product> = new Collection(products);
-console.log(`There are ${productCollection.count} products`);
+// type priceType = Product['price'];
+// type allTypes = Product[keyof Product];
 
-[...productCollection].forEach((p) =>
-	console.log(`Product: ${p.name}, ${p.price}`)
-);
+let p = new Product('Running Shoe', 100);
+console.log(getValue<Product, 'name'>(p, 'name'));
+console.log(getValue(p, 'price'));
+
+let e = new Employee('Bob', 'Director');
+console.log(getValue(e, 'name'));
+console.log(getValue(e, 'role'));
