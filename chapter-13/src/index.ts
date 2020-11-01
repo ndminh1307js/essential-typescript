afb6e1fe13294e3498b60c111a49d955331cebdf
ndminh1307js <ndminh1307js@gmail.com>
2020-11-01 08:52:13 +0700

    Chapter 13: Changing property optionality and mutability

--- chapter-13/src/index.ts
+++ chapter-13/src/index.ts
@@ -4,43 +4,26 @@ type Mapped<T> = {
 	[P in keyof T]: T[P];
 };
 
-let p: Mapped<Product> = { name: 'Kayak', price: 275 };
+type MakeOptional<T> = {
+	[P in keyof T]?: T[P];
+};
+
+type MakeRequired<T> = {
+	[P in keyof T]-?: T[P];
+};
+
+type MakeReadOnly<T> = {
+	readonly [P in keyof T]: T[P];
+};
+
+type MakeReadWrite<T> = {
+	-readonly [P in keyof T]: T[P];
+};
+
+type optionalType = MakeOptional<Product>;
+type requiredType = MakeRequired<optionalType>;
+type readonlyType = MakeReadOnly<requiredType>;
+type readwriteType = MakeReadWrite<readonlyType>;
+
+let p: readwriteType = new Product('Shoe', 125);
 console.log(p);
-
-let e: Mapped<Employee> = { name: 'Murray', role: 'Developer' };
-console.log(e);
-
-// let products = [new Product('Running Shoe', 25), new Product('Hat', 12)];
-
-// class Collection<T, K extends keyof T> implements Iterable<T> {
-// 	private items: Map<T[K], T>;
-
-// 	constructor(initialItems: T[] = [], private propertyName: K) {
-// 		this.items = new Map<T[K], T>();
-// 		this.add(...initialItems);
-// 	}
-
-// 	add(...newItems: T[]): void {
-// 		newItems.forEach((item) => this.items.set(item[this.propertyName], item));
-// 	}
-
-// 	get(key: T[K]): T {
-// 		return this.items.get(key);
-// 	}
-
-// 	get count(): number {
-// 		return this.items.size;
-// 	}
-
-// 	[Symbol.iterator](): Iterator<T> {
-// 		return this.items.values();
-// 	}
-// }
-
-// let productCollection: Collection<Product, 'name'> = new Collection(
-// 	products,
-// 	'name'
-// );
-
-// let itemByKey = productCollection.get('Hat');
-// console.log(itemByKey);
