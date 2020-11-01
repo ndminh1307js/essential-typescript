e0d7409cd820bca4719c79aafbb0fb546b2263f7
ndminh1307js <ndminh1307js@gmail.com>
2020-11-01 08:12:10 +0700

    Chapter 13: Using generic collections - Using Map

--- chapter-13/src/index.ts
+++ chapter-13/src/index.ts
@@ -5,18 +5,19 @@ let products = [new Product('Running Shoe', 25), new Product('Hat', 12)];
 type shapeType = { name: string };
 
 class Collection<T extends shapeType> {
-	private items: Set<T>;
+	private items: Map<string, T>;
 
-	constructor(initialItems: T[]) {
-		this.items = new Set<T>(initialItems);
+	constructor(initialItems: T[] = []) {
+		this.items = new Map<string, T>();
+		this.add(...initialItems);
 	}
 
 	add(...newItems: T[]): void {
-		newItems.forEach((item) => this.items.add(item));
+		newItems.forEach((newItem) => this.items.set(newItem.name, newItem));
 	}
 
 	get(name: string): T | undefined {
-		return [...this.items.values()].find((item) => item.name === name);
+		return this.items.get(name);
 	}
 
 	get count(): number {
